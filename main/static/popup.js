c728816628cda2915a1f2d38b64a5f2c76504708
Tara Cates <tara@tcates.com>
2013-11-09 15:30:02 -0800

    added tooltip for new goal on home.html instead of new_goal.html

--- main/static/popup.js
+++ main/static/popup.js
@@ -1,17 +1,42 @@
-// Create the tooltips only when document ready
- $(document).ready(function()
- {
-	// MAKE SURE YOUR SELECTOR MATCHES SOMETHING IN YOUR HTML!!!
-	$('a').each(function() {
-		$(this).qtip({
-			content: {
-                 		text: $(this).next('.tooltiptext'),
-      				title: {
-         				text: 'Create New Goal',
-         				button: 'Close' // Close button
-      				},
-             		},
-   			hide: false // Don't hide on any event except close button
-         });
-     });
- });
+function qtip_content() {
+    // MAKE SURE YOUR SELECTOR MATCHES SOMETHING IN YOUR HTML!!!
+    $('a').each(function() {
+        $(this).qtip({
+            content: {
+                text: $(this).next('.tooltiptext'),
+                title: {
+                    text: 'Create New Goal',
+                    button: 'Close' // Close button
+                },
+            },
+            hide: false // Don't hide on any event except close button
+        });
+    });
+}
+
+function register_new_goal_handler() {
+    $("#create_new_goal").submit(submit_new_goal);
+}
+
+
+function submit_new_goal() {
+    $.post("", $("#create_new_goal").serialize(), function (data, textStatus, jqXHR) {
+        if (jqXHR.getResponseHeader('X-addNewGoalStatus') == 'success') {
+            $("#new_goal_container").html(data);
+            $('a').qtip('hide');
+            register_new_goal_handler();
+            // TODO - Add in a new goal tab to calendar
+        } else {
+            $("#new_goal_container").html(data);
+            $("#new_goal_container").show();
+            register_new_goal_handler();
+        }   
+    }); 
+    return false;
+}
+
+$(function () {
+    qtip_content();
+    register_new_goal_handler();
+});
+
--- main/views.py
+++ main/views.py
@@ -37,23 +37,25 @@ def getWeeksToDisplay(selectedDate = getToday()):
 
 @login_required
 def home(request):
+
+    if request.method == 'POST':
+        return handle_new_goal_form(request)
+
     weeks = getWeeksToDisplay()
     goalentry_list = GetGoalEntryList(request.user, getToday())
-    return render(request, 'home.html', {'weekdays': WEEKDAYS, 'weeks': weeks, 'goalentry_list': goalentry_list})
+    form = NewGoalForm(initial={'startdate': datetime.date.today()})
+    return render(request, 'home.html', {'form': form, 'weekdays': WEEKDAYS, 'weeks': weeks, 'goalentry_list': goalentry_list})
 
-def new_goal(request):
-	if request.method == 'POST':
-		form = NewGoalForm(request.POST)
-		if form.is_valid():
-			new_goal = form.save(commit=False)
-			new_goal.user = request.user
-			new_goal.save()
-			response = render(request, 'home.html')
-			response['X-addNewGoalStatus'] = 'success'
-		else:
-			response = render(request, 'new_goal.html', {'form': form})
-			response['X-addNewGoalStatus'] = 'failed'
-	else:
-		form = NewGoalForm(initial={'startdate': datetime.date.today()})
-		response = render(request, 'new_goal.html', {'form': form})
-	return response
+def handle_new_goal_form(request):
+    form = NewGoalForm(request.POST)
+    if form.is_valid():
+        new_goal = form.save(commit=False)
+        new_goal.user = request.user
+        new_goal.save()
+        form = NewGoalForm(initial={'startdate': datetime.date.today()})
+        response = render(request, 'new_goal.html', {'form': form})
+        response['X-addNewGoalStatus'] = 'success'
+    else:
+        response = render(request, 'new_goal.html', {'form': form})
+        response['X-addNewGoalStatus'] = 'failed'
+    return response
