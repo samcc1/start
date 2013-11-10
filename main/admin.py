2f7e05fd53ae0d277d27b2bf86ca66b9f148f4fb
Tara Cates <tara@tcates.com>
2013-11-09 16:50:03 -0800

    added goal table to admin page

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,11 @@
-from models import GoalEntry
+from models import GoalEntry, Goal
 from django.contrib import admin
 
 class GoalEntryAdmin(admin.ModelAdmin):
 	list_display = ["user"]
 
+class GoalAdmin(admin.ModelAdmin):
+	list_display = ["user", "title", "startdate", "enddate", "icon"]
+
 admin.site.register(GoalEntry, GoalEntryAdmin)
+admin.site.register(Goal, GoalAdmin)
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('main.views',
-	url(r'^new_goal/', 'new_goal'),
 	url(r'',    'home'),
-	
 )
 
